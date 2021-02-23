e1f82e0b9c2219da03553f84103e0477daf5f2ce
cuihaoran <1054346513@qq.com>
2021-02-23 17:18:37 +0800

    promise

--- JavaScript/Q8.Promise.js
+++ JavaScript/Q8.Promise.js
@@ -100,5 +100,105 @@ p.then(function(res) {
 
 /********************************* start *********************************/
 
-const
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+function Promise(excutor) {
+  let that = this;  // 缓存当前promise实例对象
+  this.status = PENDING;  // 初始状态
+  this.value = undefined;   // fulfilled状态时 返回的信息
+  this.reason = undefined;  // rejected状态时 拒绝的原因
+  this.onFulfilledCallbacks = [];  // 存储fulfilled状态对应的onFulfilled函数
+  this.onRejectedCallbacks = [];
+  function resolve(value) {   // value 成功态时接收的终值
+    if(value instanceof Promise) {
+      return value.then(resolve, reject);
+    }
+    // 实践中要确保 onFulfilled 和 onRejected ⽅方法异步执行，且应该在 then ⽅法被调⽤用的那一轮事件循环之后的新执⾏栈中执⾏。
+    setTimeout(() => {
+      // 调用resolve回调对应的onFulfilled函数
+      if(that.status === PENDING) {
+        // 只能由pending ----> fulfilled状态
+        that.status = FULFILLED;
+        that.value = value;
+        that.onFulfilledCallbacks.forEach(cb => cb(that.value));
+      }
+    });
+  }
 
+  function reject(reason) {   // reason 失败状态时接收的原因
+    setTimeout(() =>{
+      // 调⽤用reject 回调对应onRejected函数
+      if(that.status === PENDING) {
+        // 只能由pending状态变为rejected状态
+        that.status = REJECTED;
+        that.reason = reason;
+        that.onRejectedCallbacks.forEach(cb => cb(that.reason));
+      }
+    });
+  }
+
+  // 捕获在excutor执行器中抛出的异常
+  try {
+    excutor(resolve, reject);
+  }catch(err) {
+    reject(err);
+  }
+}
+
+Promise.prototype.then = function(onFulfilled, onRejected) {
+  const that = this;
+  let newPromise;
+  // 处理参数默认值，确保参数后续能够继续执行
+  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+  onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason };
+
+  if(that.status === FULFILLED) {
+    return newPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          let x = onFulfilled(that.value);
+          resolvePromise(newPromise, x, resolve, reject);
+        }catch(err) {
+          // 捕获前⾯面onFulfilled中抛出的异常then(onFulfilled, onRejected);
+          reject(err);
+        }
+      })
+    })
+  }
+
+  if(that.status === REJECTED) {  // 失败状态
+    return newPromise = new Promise((resolve, reject) =>{
+      setTimeout(() => {
+        try {
+          let x = onRejected(that.reason);
+          resolvePromise(newPromise, x, resolve, reject);
+        } catch(e) {
+          reject(e);
+        }
+      })
+    })
+  }
+
+  if(that.status === PENDING) {
+    return newPromise = new Promise((resolve, reject) => {
+      that.onFulfilledCallbacks.push((value) =>{
+        try{
+          let x = onFulfilled(value);
+          resolvePromise(newPromise, x, resolve, reject);
+        } catch(err) {
+          reject(err);
+        }
+      });
+
+      that.onRejectedCallbacks.push((reason) => {
+        try{
+          let x = onRejected(reason);
+          resolvePromise(newPromise, x, resolve, reject);
+        }catch(err) {
+          reject(err);
+        }
+      })
+    })
+  }
+}
