ae284d213b036447e3b2823852b5e2344cb99c4e
RenektonChr <1054346513@qq.com>
2021-02-23 21:15:06 +0800

    promise

---
+++ JavaScript/Q8.promise.js
@@ -0,0 +1,14 @@
+// 简单版本
+class Promise {
+  constructor(executor) {
+    this.state = 'pending';
+    this.value = undefined;
+    this.reason = undefined;
+
+    let resolve = value => {
+      if(this.state === 'pending') {
+        this.state = 
+      }
+    }
+  }
+}
